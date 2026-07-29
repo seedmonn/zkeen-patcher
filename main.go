d2440d766e6fd5c569b9d03e06206273e9df01e2
seedmonn <47562825+seedmonn@users.noreply.github.com>
2026-07-29 15:56:16 +0300

    add google ai

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ var extraDomains = []string{"rulate.ru", "hentailib.me", "ranobelib.me", "bllate
 var extraIPs = []string{"77.105.169.97"}
 
 // Additional dlc.dat sections to inject into DOMAINS (by name)
-var injectSections = []string{"EHENTAI"}
+var injectSections = []string{"EHENTAI", "GOOGLE-DEEPMIND"}
 
 var dropSections = map[string]bool{
 	"BYPASS": true, "CN": true, "RU": true,
